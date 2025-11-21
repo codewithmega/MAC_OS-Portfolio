f33b0eeb57a05db935807e33d1aaaebe2dbd0b26
Timothy404 <99612496+Timothy404@users.noreply.github.com>
2025-11-21 21:55:56 +0100

    dock implementation

--- src/constants/index.js
+++ src/constants/index.js
@@ -23,6 +23,45 @@ const navIcons = [
         img: "/public/icons/mode.png",
     },
 
+];
+
+const dockApps = [
+    {
+        id: "finder",
+        name: "Portfolio",
+        icon: "finder.png",
+        canOpen: true,
+    },
+    {
+        id: "safari",
+        name: "Articles",
+        icon: "safari.png",
+        canOpen: true,
+    },
+    {
+        id: "photos",
+        name: "Gallery",
+        icon: "photos.png",
+        canOpen: true,
+    },
+    {
+        id: "contact",
+        name: "Contact",
+        icon: "contact.png",
+        canOpen: true,
+    },
+    {
+        id: "terminal",
+        name: "Skills",
+        icon: "terminal.png",
+        canOpen: true,
+    },
+    {
+        id: "trash",
+        name: "Archive",
+        icon: "trash.png",
+        canOpen: false,
+    },
 ]
 
-export { navLinks, navIcons };
+export { navLinks, navIcons, dockApps };
